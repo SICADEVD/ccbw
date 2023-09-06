2dcced4b06877a0121c2c49dcc6aa1b06fc5175e
jatachi <anguijathi@gmail.com>
2023-09-06 11:33:27 +0000

    creation de l'apigetsection

---
+++ core/app/Http/Controllers/ApisectionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Section;
+use Illuminate\Http\Request;
+
+class ApisectionController extends Controller
+{
+    public function getsections()
+    {
+        $sections = Section::orderBy('created_at','desc')->with('localite')->get();
+        return response()->json($sections,201);
+    }
+}
--- core/routes/api.php
+++ core/routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\ApiestimationController;
 use App\Http\Controllers\ApissrteclrmsController; 
 use App\Http\Controllers\ApiapplicationController;
 use App\Http\Controllers\ApievaluationController;
-
+use App\Http\Controllers\ApisectionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -67,4 +67,8 @@ Route::match(['POST'],'apievaluation', [ApievaluationController::class, 'store']
 Route::match(['POST'],'getquestionnaire', [ApievaluationController::class, 'getQuestionnaire']); 
 Route::match(['POST'],'getnotation', [ApievaluationController::class, 'getNotation']); 
 Route::match(['POST'],'getcampagne', [AuthController::class, 'getCampagne']);
+
+//route pour la gestion des sections
+
+Route::match(['POST'],'getsections',[ApisectionController::class, 'getsections']);
 
