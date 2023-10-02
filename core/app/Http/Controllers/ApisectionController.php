4d47580e412498884f5db01f7c44985ba8efcb9f
jatachi <anguijathi@gmail.com>
2023-10-02 11:22:53 +0000

    update

--- core/app/Http/Controllers/ApisectionController.php
+++ core/app/Http/Controllers/ApisectionController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Section;
+use App\Models\Cooperative;
 use Illuminate\Http\Request;
 
 class ApisectionController extends Controller
 {
     public function getsections()
     {
-        $sections = Section::orderBy('created_at','desc')->with('cooperative')->get();
+        $manager   = auth()->user();
+        $sections = Section::where('cooperative_id',$manager->cooperative_id)->with('cooperative')->get();
         return response()->json($sections,201);
     }
 }
