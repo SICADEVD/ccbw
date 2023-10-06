2e0cdf386f81caa66ae93a3df17e5054afd590ed
jatachi <anguijathi@gmail.com>
2023-10-06 13:29:42 +0000

    update section

--- core/app/Http/Controllers/Manager/SectionController.php
+++ core/app/Http/Controllers/Manager/SectionController.php
@@ -18,7 +18,9 @@ class SectionController extends Controller
         $pageTitle = "Gestion des sections"; 
         $manager   = auth()->user();
         $cooperatives = Cooperative::active()->where('id',$manager->cooperative_id)->get();
-        $sections = Section::orderBy('created_at','desc')->with('cooperative')->paginate(getPaginate());
+        // $sections = Section::orderBy('created_at','desc')->with('cooperative')->paginate(getPaginate());
+
+        $sections = Section::latest('id')->joinRelationship('cooperative')->where('cooperative_id',$manager->cooperative_id)->with('cooperative')->paginate(getPaginate());
         
         return view('manager.section.index',compact('pageTitle','cooperatives','sections'));
     }
