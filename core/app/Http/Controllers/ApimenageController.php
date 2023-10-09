ff868fb5aa529644fff352f23f4965289895fae6
jatachi <anguijathi@gmail.com>
2023-10-09 18:45:51 +0000

    update apiMenage update

--- core/app/Http/Controllers/ApimenageController.php
+++ core/app/Http/Controllers/ApimenageController.php
@@ -47,6 +47,43 @@ class ApimenageController extends Controller
 
         if($request->id !=null) {
             $menage = Menage::find($request->id);
+            $validationRule = [
+                'producteur'    => 'required|exists:producteurs,id',
+                'quartier' => 'required|max:255',
+                'ageEnfant0A5' => ['required','integer', new VlidateEnfantTotal],
+                'ageEnfant6A17' => ['required','integer', new VlidateEnfantTotal],
+                'enfantscolarises' => ['required','integer', new VlidateEnfantTotal],
+                'enfantsPasExtrait' => ['required','integer', new VlidateEnfantTotal],
+                'sources_energies'  => 'required|max:255',
+                'ordures_menageres'  => 'required|max:255',
+                'separationMenage'  => 'required|max:255',
+                'eauxToillette'  => 'required|max:255',
+                'eauxVaisselle'  => 'required|max:255',
+                'wc'  => 'required|max:255',
+                'sources_eaux'  => 'required|max:255',
+                'equipements'  => 'required|max:255',
+                'traitementChamps'  => 'required|max:255',
+                'activiteFemme'  => 'required|max:255',
+            ];
+            $validationMessage = [
+                'producteur.required' => 'Le champ producteur est obligatoire',
+                'quartier.required' => 'Le champ quartier est obligatoire',
+                'ageEnfant0A5.required' => 'Le champ ageEnfant0A5 est obligatoire',
+                'ageEnfant6A17.required' => 'Le champ ageEnfant6A17 est obligatoire',
+                'enfantscolarises.required' => 'Le champ enfantscolarises est obligatoire',
+                'enfantsPasExtrait.required' => 'Le champ enfantsPasExtrait est obligatoire',
+                'sources_energies.required' => 'Le champ sources_energies est obligatoire',
+                'ordures_menageres.required' => 'Le champ ordures_menageres est obligatoire',
+                'separationMenage.required' => 'Le champ separationMenage est obligatoire',
+                'eauxToillette.required' => 'Le champ eauxToillette est obligatoire',
+                'eauxVaisselle.required' => 'Le champ eauxVaisselle est obligatoire',
+                'wc.required' => 'Le champ wc est obligatoire',
+                'sources_eaux.required' => 'Le champ sources_eaux est obligatoire',
+                'equipements.required' => 'Le champ equipements est obligatoire',
+                'traitementChamps.required' => 'Le champ traitementChamps est obligatoire',
+                'activiteFemme.required' => 'Le champ activiteFemme est obligatoire',
+            ];
+
         } else {
             $menage = new Menage(); 
             $validationRule = [
@@ -93,7 +130,7 @@ class ApimenageController extends Controller
                 return response()->json("Ce producteur a déjà un menage enregistré", 501);
             }
         }
-        $menage->producteur_id  = $request->producteur;  
+        $menage->producteur_id  = $request->producteur_id;  
         $menage->quartier  = $request->quartier;
         $menage->ageEnfant0A5  = $request->ageEnfant0A5;
         $menage->ageEnfant6A17  = $request->ageEnfant6A17;
