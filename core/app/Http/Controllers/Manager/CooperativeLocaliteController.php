f7eae25e8cbbfa1289d6394389af0277c017d17f
jatachi <anguijathi@gmail.com>
2023-10-17 07:41:51 +0000

    update localite index

--- core/app/Http/Controllers/Manager/CooperativeLocaliteController.php
+++ core/app/Http/Controllers/Manager/CooperativeLocaliteController.php
@@ -20,20 +20,36 @@ class CooperativeLocaliteController extends Controller
 
     public function index()
     {
-        $pageTitle      = "Gestion des localités"; 
-        $manager   = auth()->user();
-        $cooperativeLocalites = Localite::searchable(['localites.nom', 'localites.codeLocal', 'localites.type_localites', 'localites.sousprefecture', 'sections.libelle'])->latest('id')->joinRelationship('section')->where('cooperative_id',$manager->cooperative_id)->paginate(getPaginate());
-        $cooperatives = Cooperative::active()->where('id',$manager->cooperative_id)->get();
+        // $pageTitle      = "Gestion des localités";
+        // $manager   = auth()->user();
+        // $cooperativeLocalites = Localite::searchable(['localites.nom', 'localites.codeLocal', 'localites.type_localites', 'localites.sousprefecture', 'sections.libelle'])->latest('id')->joinRelationship('section')->where('cooperative_id', $manager->cooperative_id)->paginate(getPaginate());
+        // $cooperatives = Cooperative::active()->where('id', $manager->cooperative_id)->get();
+        // $sections = Section::all();
+        // return view('manager.localite.index', compact('pageTitle', 'cooperativeLocalites', 'cooperatives', 'sections'));
+
+
+        $pageTitle = "Gestion des localités";
+        $manager = auth()->user();
+
+        $cooperativeLocalites = Localite::searchable(['localites.nom', 'localites.codeLocal', 'localites.type_localites', 'localites.sousprefecture', 'sections.libelle'])
+            ->latest('id')
+            ->joinRelationship('section')
+            ->with('section.cooperative') // Préchargez la relation "cooperative" ici
+            ->where('cooperative_id', $manager->cooperative_id)
+            ->paginate(getPaginate());
+
+        $cooperatives = Cooperative::active()->where('id', $manager->cooperative_id)->get();
         $sections = Section::all();
-        return view('manager.localite.index', compact('pageTitle', 'cooperativeLocalites','cooperatives','sections'));
+
+        return view('manager.localite.index', compact('pageTitle', 'cooperativeLocalites', 'cooperatives', 'sections'));
     }
 
     public function create()
     {
         $pageTitle = "Ajouter une localité";
         $manager   = auth()->user();
-        $sections = Section::where('cooperative_id',$manager->cooperative_id)->get();
-        return view('manager.localite.create', compact('pageTitle', 'sections','manager'));
+        $sections = Section::where('cooperative_id', $manager->cooperative_id)->get();
+        return view('manager.localite.create', compact('pageTitle', 'sections', 'manager'));
     }
 
     public function store(Request $request)
@@ -53,7 +69,7 @@ class CooperativeLocaliteController extends Controller
         ];
 
         $manager   = auth()->user();
- 
+
 
         $request->validate($validationRule);
 
@@ -63,14 +79,14 @@ class CooperativeLocaliteController extends Controller
             $notify[] = ['error', 'Cette coopérative est désactivé'];
             return back()->withNotify($notify)->withInput();
         }
-        
-        if($request->id) {
+
+        if ($request->id) {
             $localite = Localite::findOrFail($request->id);
             $message = "La localité a été mise à jour avec succès";
         } else {
-            $localite           = new Localite(); 
+            $localite           = new Localite();
             $localite->nom = $this->verifylocalite($request->nom);
-        } 
+        }
         $localite->section_id = $request->section_id;
         $localite->userid = $manager->id;
         $localite->nom = $request->nom;
@@ -96,109 +112,113 @@ class CooperativeLocaliteController extends Controller
         $localite->associationFemmes    = $request->associationFemmes;
         $localite->associationJeunes    = $request->associationJeunes;
         $localite->localongitude    = $request->localongitude;
-        $localite->localatitude    = $request->localatitude; 
-         
-        $localite->codeLocal    = isset($request->codeLocal) ? $request->codeLocal : $this->generelocalitecode($request->nom); 
+        $localite->localatitude    = $request->localatitude;
+
+        $localite->codeLocal    = isset($request->codeLocal) ? $request->codeLocal : $this->generelocalitecode($request->nom);
         $localite->save();
 
-        if($localite !=null ){
+        if ($localite != null) {
 
             $id = $localite->id;
-            
-              if(($request->nomecolesprimaires !=null) && $request->nombrecole !=null) {
 
-                $verification   = Localite_ecoleprimaire::where('localite_id',$id)->get();
-            if($verification->count()){ 
-                DB::table('localite_ecoleprimaires')->where('localite_id',$id)->delete();
-            }
-                $i=0;
-                
-                foreach($request->nomecolesprimaires as $data){
-                    if($data !=null)
-                    {
-                        DB::table('localite_ecoleprimaires')->insert(['localite_id'=>$id,'nomecole'=>$data]);
-                    } 
-                  $i++;
+            if (($request->nomecolesprimaires != null) && $request->nombrecole != null) {
+
+                $verification   = Localite_ecoleprimaire::where('localite_id', $id)->get();
+                if ($verification->count()) {
+                    DB::table('localite_ecoleprimaires')->where('localite_id', $id)->delete();
                 }
+                $i = 0;
 
+                foreach ($request->nomecolesprimaires as $data) {
+                    if ($data != null) {
+                        DB::table('localite_ecoleprimaires')->insert(['localite_id' => $id, 'nomecole' => $data]);
+                    }
+                    $i++;
+                }
             }
-          }
+        }
 
         $notify[] = ['success', isset($message) ? $message : 'La localité a été crée avec succès.'];
         return back()->withNotify($notify);
     }
 
-    private function verifylocalite($nom){
+    private function verifylocalite($nom)
+    {
         $action = 'non';
-        do{
-        $data = Localite::select('nom')->where('nom',$nom)->orderby('id','desc')->first();
-        if($data !=''){
+        do {
+            $data = Localite::select('nom')->where('nom', $nom)->orderby('id', 'desc')->first();
+            if ($data != '') {
 
-            $nomLocal = $data->nom; 
-            $nom = Str::beforeLast($nomLocal,' ');
-        $chaine_number = Str::afterLast($nomLocal,' '); 
-       
-        if(is_numeric($chaine_number) && ($chaine_number<10)){$zero="00";}
-        else if(is_numeric($chaine_number) && ($chaine_number<100)){$zero="0";}
-        else{$zero="00";
-            $chaine_number=0;} 
-           
-        $sub=$nom.' ';
-        $lastCode=$chaine_number+1;
-        $nomLocal=$sub.$zero.$lastCode; 
+                $nomLocal = $data->nom;
+                $nom = Str::beforeLast($nomLocal, ' ');
+                $chaine_number = Str::afterLast($nomLocal, ' ');
 
-        }else{
-           
-            $nomLocal=$nom;
-        }
-        $verif = Localite::select('nom')->where('nom',$nomLocal)->orderby('id','desc')->first(); 
-        if($verif ==null){
-            $action = 'non';
-        }else{
-            $action = 'oui';
-            $nom = $verif->nom;
-        }
-        
-        }while($action !='non');
+                if (is_numeric($chaine_number) && ($chaine_number < 10)) {
+                    $zero = "00";
+                } else if (is_numeric($chaine_number) && ($chaine_number < 100)) {
+                    $zero = "0";
+                } else {
+                    $zero = "00";
+                    $chaine_number = 0;
+                }
 
-    return $nomLocal;
+                $sub = $nom . ' ';
+                $lastCode = $chaine_number + 1;
+                $nomLocal = $sub . $zero . $lastCode;
+            } else {
+
+                $nomLocal = $nom;
+            }
+            $verif = Localite::select('nom')->where('nom', $nomLocal)->orderby('id', 'desc')->first();
+            if ($verif == null) {
+                $action = 'non';
+            } else {
+                $action = 'oui';
+                $nom = $verif->nom;
+            }
+        } while ($action != 'non');
+
+        return $nomLocal;
     }
-    
+
     private function generelocalitecode($name)
     {
         $action = 'non';
-        do{
+        do {
 
-        $data = Localite::select('codeLocal')->where('nom',$name)->orderby('id','desc')->first();
+            $data = Localite::select('codeLocal')->where('nom', $name)->orderby('id', 'desc')->first();
 
-        if($data !=''){
+            if ($data != '') {
 
-            $code = $data->codeLocal;
+                $code = $data->codeLocal;
 
-        $chaine_number = Str::afterLast($code,'-');
+                $chaine_number = Str::afterLast($code, '-');
 
-        if($chaine_number<10){$zero="00";}
-        else if($chaine_number<100){$zero="0";}
-        else{$zero="";}
-        }else{
-            $zero="00";
-            $chaine_number=0;
-        }
+                if ($chaine_number < 10) {
+                    $zero = "00";
+                } else if ($chaine_number < 100) {
+                    $zero = "0";
+                } else {
+                    $zero = "";
+                }
+            } else {
+                $zero = "00";
+                $chaine_number = 0;
+            }
 
-        $abrege=Str::upper(Str::substr($name,0,3));
-        $sub=$abrege.'-';
-        $lastCode=$chaine_number+1;
-        $codeP=$sub.$zero.$lastCode;
+            $abrege = Str::upper(Str::substr($name, 0, 3));
+            $sub = $abrege . '-';
+            $lastCode = $chaine_number + 1;
+            $codeP = $sub . $zero . $lastCode;
 
-        $verif = Localite::select('nom')->where('codeLocal',$codeP)->orderby('id','desc')->first();
-        if($verif ==null){
-            $action = 'non';
-        }else{
-            $action = 'oui';
-            $name = $verif->nom;
-        }
-
-        }while($action !='non');
+            $verif = Localite::select('nom')->where('codeLocal', $codeP)->orderby('id', 'desc')->first();
+            if ($verif == null) {
+                $action = 'non';
+            } else {
+                $action = 'oui';
+                $name = $verif->nom;
+            }
+        } while ($action != 'non');
 
         return $codeP;
     }
@@ -208,9 +228,9 @@ class CooperativeLocaliteController extends Controller
         $pageTitle = "Mise à jour de la localité";
         $manager   = auth()->user();
         $localite   = Localite::findOrFail($id);
-        $sections = Section::where('cooperative_id',$manager->cooperative_id)->get();
-        return view('manager.localite.edit', compact('pageTitle', 'sections', 'localite','manager'));
-    } 
+        $sections = Section::where('cooperative_id', $manager->cooperative_id)->get();
+        return view('manager.localite.edit', compact('pageTitle', 'sections', 'localite', 'manager'));
+    }
 
     public function status($id)
     {
