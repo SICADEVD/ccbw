d4131435a7384a39b8b509125fabcf86ec373ada
jatachi <anguijathi@gmail.com>
2023-11-06 17:36:07 +0000

    update localite

---
+++ core/app/Models/Localite_jour_marche.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Localite_jour_marche extends Model
+{
+    use HasFactory;
+}
