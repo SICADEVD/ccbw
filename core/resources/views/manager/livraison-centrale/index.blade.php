7125663cd278b4770c22002e98c7b06d701215ae
jatachi <anguijathi@gmail.com>
2023-12-08 14:55:39 +0000

    update

--- core/resources/views/manager/livraison-centrale/index.blade.php
+++ core/resources/views/manager/livraison-centrale/index.blade.php
@@ -116,7 +116,7 @@
 @push('breadcrumb-plugins') 
 
 <a href="{{ route('manager.livraison.stock.section.create') }}" class="btn  btn-outline--primary h-45 addNewCooperative">
-        <i class="las la-plus"></i>@lang("Enregistrer Livraison vers Magasin Central")
+        <i class="las la-plus"></i>@lang("Enregistrer Connaissement vers Usine")
     </a>
 <a href="{{ route('manager.livraison.exportExcel.livraisonAll') }}" class="btn  btn-outline--warning h-45"><i class="las la-cloud-download-alt"></i> Exporter en Excel</a>
 @endpush
