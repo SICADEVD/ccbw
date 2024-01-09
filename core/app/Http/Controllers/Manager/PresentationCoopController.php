bfd4d04b3f89876822615470d59c023089118755
jatachi <anguijathi@gmail.com>
2024-01-09 16:54:08 +0000

    update presentation coop

--- core/app/Http/Controllers/Manager/PresentationCoopController.php
+++ core/app/Http/Controllers/Manager/PresentationCoopController.php
@@ -3,13 +3,55 @@
 namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
+use App\Models\Producteur;
 use Illuminate\Http\Request;
 
 class PresentationCoopController extends Controller
 {
     public function index()
     {
-        return view('manager.presentation-coop.index');
+        $nombreProducteur = Producteur::count();
+        $hommes = Producteur::where('sexe', 'Homme')->count();
+        $femmes = Producteur::where('sexe', 'Femme')->count();
+
+        $countProducteursRainforest = Producteur::whereHas('certifications', function ($query) {
+            $query->where('certification', 'RAINFOREST');
+        })->count();
+
+        $countProducteursRainforestHomme = Producteur::where('sexe', 'Homme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'RAINFOREST');
+        })->count();
+
+        $countProducteursRainforestFemme = Producteur::where('sexe', 'Femme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'RAINFOREST');
+        })->count();
+
+        $countProducteursFairtrade = Producteur::whereHas('certifications', function ($query) {
+            $query->where('certification', 'FAIRTRADE');
+        })->count();
+
+        $countProducteursFairtradeHomme = Producteur::where('sexe', 'Homme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'FAIRTRADE');
+        })->count();
+
+        $countProducteursFairtradeFemme = Producteur::where('sexe', 'Femme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'FAIRTRADE');
+        })->count();
+
+        $countProducteursBio = Producteur::whereHas('certifications', function ($query) {
+            $query->where('certification', 'BIO');
+        })->count();
+
+        $countProducteursBioHomme = Producteur::where('sexe', 'Homme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'BIO');
+        })->count();
+
+        $countProducteursBioFemme = Producteur::where('sexe', 'Femme')->whereHas('certifications', function ($query) {
+            $query->where('certification', 'BIO');
+        })->count();
+        
+
+        return view('manager.presentation-coop.index', compact('nombreProducteur', 'hommes', 'femmes', 'countProducteursRainforest', 'countProducteursFairtrade', 'countProducteursBio', 'countProducteursRainforestHomme', 'countProducteursRainforestFemme', 'countProducteursFairtradeHomme', 'countProducteursFairtradeFemme', 'countProducteursBioHomme', 'countProducteursBioFemme'));
     }
 
     public function create()
--- core/resources/views/manager/presentation-coop/index.blade.php
+++ core/resources/views/manager/presentation-coop/index.blade.php
@@ -1,17 +1,286 @@
 @extends('manager.layouts.app')
 @section('panel')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card b-radius--10 mb-3">
-                <div class="card-body">
-                  
-                </div>
-            </div>
-          
-        </div>
-    </div>
+    <div class="row mb-none-30">
+        <div class="row">
+            <div class="col-md-6">
+                <table>
+                    <h3>Evalution des membres</h3>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>2023</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center" colspan="2">Membre-Adhérent</td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td>{{$hommes}}</td>
+                        </tr>
+                        <tr>
+                            <td>Femmes</td>
+                            <td>{{ $femmes }}</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>{{ $nombreProducteur}}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center" colspan="2">Répartition des membres par programme </td>
+                        </tr>
+                        <tr>
+                            <td>Membres Certifié RA </td>
+                            <td> {{$countProducteursRainforest}} </td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td> {{$countProducteursRainforestHomme}} </td>
+                        </tr>
+                        <tr>
+                            <td>Femmes</td>
+                            <td> {{$countProducteursRainforestFemme}} </td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td> {{$countProducteursRainforest}} </td>
+                        </tr>
+                        <tr>
+                            <td>Membres Certifié FT </td>
+                            <td> {{$countProducteursFairtrade}} </td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td> {{ $countProducteursFairtradeHomme }} </td>
 
-    <x-confirmation-modal />
+                        </tr>
+                        <tr>
+                            <td>Femmes</td>
+                            <td> {{$countProducteursFairtradeFemme}} </td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td> {{$countProducteursFairtrade}} </td>
+                        </tr>
+                        <tr>
+                            <td>Membres Certifié BIO </td>
+                            <td> {{ $countProducteursBio }} </td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td>  {{ $countProducteursBioHomme }} </td>
+                        </tr>
+                        <tr>
+                            <td>Femme</td>
+                            <td> {{ $countProducteursBioFemme }} </td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>{{ $countProducteursBio }}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center" colspan="2">Membres Autres Programmes Durabilité </td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Femme</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center" colspan="2">Membres Ordinaire</td>
+                        </tr>
+                         <tr>
+                            <td>Homme</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Femme</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <table>
+                    <h3>Evolution de la production des membres</h3>
+                    <thead>
+                        <tr>
+                            <th>Cacao</th>
+                            <th>2023</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Superficie(Ha)</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Production (Kg)</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié RA</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié FT</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié Bio</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Programme Durabilité </td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Conventionnel/Ordinaire</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Vente (Kg)</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié RA</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié FT</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Certifié Bio</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Programme Durabilité </td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Conventionnel/Ordinaire</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-6">
+                {{-- <table>
+                    <caption>Statistiques des Membres</caption>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>2023</th>
+                            <th>2024</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Membres-Adhérent</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Femme</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Répartition des membres par programme </td>
+                            <td>Insertion d</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                    </tbody>
+                </table> --}}
+            </div>
+            <div class="col-md-6">
+                {{-- <table>
+                    <caption>Statistiques des Membres</caption>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>2023</th>
+                            <th>2024</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Membres-Adhérent</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Homme</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Femme</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>Insertion des données ici</td>
+                            <td>Insertion des données ici</td>
+                        </tr>
+                    </tbody>
+                </table> --}}
+            </div>
+        </div>
+        <x-confirmation-modal />
+    </div>
 @endsection
 
+@push('style')
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+            margin: 20px 0;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
 
+        th,
+        td {
+            padding: 12px;
+            text-align: center;
+            border: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #4caf50;
+            color: #fff;
+        }
+
+        caption {
+            font-size: 1.5em;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+    </style>
+@endpush
