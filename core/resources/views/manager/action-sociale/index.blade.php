55d53eced059e5c39f12b8fffbdbf7c420e54df3
jatachi <anguijathi@gmail.com>
2024-04-14 14:16:55 +0000

    update indexActionSociale

--- core/resources/views/manager/action-sociale/index.blade.php
+++ core/resources/views/manager/action-sociale/index.blade.php
@@ -2,11 +2,11 @@
 @section('panel')
     <div class="row">
         <div class="col-lg-12">
-        <div class="card b-radius--10 mb-3">
+            <div class="card b-radius--10 mb-3">
                 <div class="card-body">
                     <form action="">
                         <div class="d-flex flex-wrap gap-4">
-                            <input type="hidden" name="table" value="applications"/>
+                            <input type="hidden" name="table" value="applications" />
                             <div class="flex-grow-1">
                                 <label>@lang('Recherche par Mot(s) clé(s)')</label>
                                 <input type="text" name="search" value="{{ request()->search }}" class="form-control">
@@ -15,15 +15,17 @@
                                 <label>@lang('Localité')</label>
                                 <select name="localite" class="form-control">
                                     <option value="">@lang('Toutes')</option>
-                                   
+
                                 </select>
-                            </div> 
+                            </div>
                             <div class="flex-grow-1">
                                 <label>@lang('Date')</label>
-                                <input name="date" type="text" class="dates form-control" placeholder="@lang('Date de début - Date de fin')" autocomplete="off" value="{{ request()->date }}">
+                                <input name="date" type="text" class="dates form-control"
+                                    placeholder="@lang('Date de début - Date de fin')" autocomplete="off" value="{{ request()->date }}">
                             </div>
                             <div class="flex-grow-1 align-self-end">
-                                <button class="btn btn--primary w-100 h-45"><i class="fas fa-filter"></i> @lang('Filter')</button>
+                                <button class="btn btn--primary w-100 h-45"><i class="fas fa-filter"></i>
+                                    @lang('Filter')</button>
                             </div>
                         </div>
                     </form>
@@ -34,15 +36,15 @@
                     <div class="table-responsive--sm table-responsive">
                         <table class="table table--light style--two">
                             <thead>
-                                <tr> 
-                                    <th>@lang('Cooperative')</th> 
+                                <tr>
+                                    <th>@lang('Cooperative')</th>
                                     <th>@lang('Code')</th>
-                                    <th>@lang('Type projet')</th> 
-                                    <th>@lang('Titre')</th> 
-                                    <th>@lang('Date demarrage')</th> 
-                                    <th>@lang("Date fin projet")</th>
-                                    <th>@lang("Coût projet")</th>
-                                    <th>@lang("Niveau realisation")</th>
+                                    <th>@lang('Type projet')</th>
+                                    <th>@lang('Titre')</th>
+                                    <th>@lang('Date demarrage')</th>
+                                    <th>@lang('Date fin projet')</th>
+                                    <th>@lang('Coût projet')</th>
+                                    <th>@lang('Niveau realisation')</th>
                                     <th>@lang('Status')</th>
                                     <th>@lang('Action')</th>
                                 </tr>
@@ -56,47 +58,51 @@
                                         <td>
                                             <span class="fw-bold">{{ $action->code }}</span>
                                         </td>
-                                        <td> 
+                                        <td>
                                             <span class="small">
-                                            {{ $action->type_projet }} 
-                                            </span>
-                                        </td> 
-                                        <td> 
-                                            <span class="small">
-                                            {{ $action->titre_projet }} 
+                                                {{ $action->type_projet }}
                                             </span>
                                         </td>
-                                        <td> 
-                                            <span>{{ diffForHumans($action->date_demarrage) }}</span>
+                                        <td>
+                                            <span class="small">
+                                                {{ $action->titre_projet }}
+                                            </span>
                                         </td>
-                                        <td> 
+                                        <td>
+                                            <span>{{ $action->date_demarrage }}</span>
+                                        </td>
+                                        <td>
                                             <span>{{ diffForHumans($action->date_fin_projet) }}</span>
                                         </td>
-                                        <td> 
+                                        <td>
                                             <span class="small">
-                                            {{ $action->cout_projet }} 
+                                                {{ number_format($action->cout_projet, 0, ',', ' ') }}
                                             </span>
                                         </td>
-                                        <td> 
+                                        <td>
                                             <span class="small">
-                                            {{ $action->niveau_realisation }} 
+                                                {{ $action->niveau_realisation }}
                                             </span>
                                         </td>
-                                        
- 
+
+
                                         <td> @php echo $action->statusBadge; @endphp </td>
                                         <td>
-                                        <a href="{{ route('manager.communaute.action.sociale.index', ['download'=> encrypt($action->id)]) }}" class="btn btn-sm btn--danger"><i class="la la-file-pdf-o"></i>@lang('PDF')</a>
-                                        <a href="{{ route('manager.communaute.action.sociale.show', $action->id) }}"
-                                        class="btn btn-sm btn-outline--info"><i class="las la-file-invoice"></i>@lang('Détail')</a>
-                                          
-                                            <button type="button" class="btn btn-sm btn-outline--primary" data-bs-toggle="dropdown" aria-expanded="false"><i
+                                            <a href="{{ route('manager.communaute.action.sociale.index', ['download' => encrypt($action->id)]) }}"
+                                                class="btn btn-sm btn--danger"><i
+                                                    class="la la-file-pdf-o"></i>@lang('PDF')</a>
+                                            <a href="{{ route('manager.communaute.action.sociale.show', $action->id) }}"
+                                                class="btn btn-sm btn-outline--info"><i
+                                                    class="las la-file-invoice"></i>@lang('Détail')</a>
+
+                                            <button type="button" class="btn btn-sm btn-outline--primary"
+                                                data-bs-toggle="dropdown" aria-expanded="false"><i
                                                     class="las la-ellipsis-v"></i>@lang('Action')
-                                             </button>
+                                            </button>
                                             <div class="dropdown-menu p-0">
                                                 <a href="{{ route('manager.communaute.action.sociale.edit', $action->id) }}"
-                                                    class="dropdown-item"><i class="la la-pen"></i>@lang('Edit')</a> 
-                                                
+                                                    class="dropdown-item"><i class="la la-pen"></i>@lang('Edit')</a>
+
                                                 @if ($action->status == Status::DISABLE)
                                                     <button type="button" class="confirmationBtn  dropdown-item"
                                                         data-action="{{ route('manager.communaute.action.sociale.status', $action->id) }}"
@@ -109,8 +115,8 @@
                                                         data-question="@lang('Are you sure to disable this application?')">
                                                         <i class="la la-eye-slash"></i> @lang('Désactivé')
                                                     </button>
-                                                @endif 
-                                                
+                                                @endif
+
                                             </div>
                                         </td>
                                     </tr>
@@ -136,11 +142,12 @@
 @endsection
 
 @push('breadcrumb-plugins')
-    
-    <a href="{{ route('manager.communaute.action.sociale.create') }}" class="btn  btn-outline--primary h-45 addNewCooperative">
-        <i class="las la-plus"></i>@lang("Ajouter nouveau")
+    <a href="{{ route('manager.communaute.action.sociale.create') }}"
+        class="btn  btn-outline--primary h-45 addNewCooperative">
+        <i class="las la-plus"></i>@lang('Ajouter nouveau')
     </a>
-    <a href="{{ route('manager.communaute.action.sociale.exportExcel.actionSocialeAll') }}" class="btn  btn-outline--warning h-45"><i class="las la-cloud-download-alt"></i> Exporter en Excel</a>
+    <a href="{{ route('manager.communaute.action.sociale.exportExcel.actionSocialeAll') }}"
+        class="btn  btn-outline--warning h-45"><i class="las la-cloud-download-alt"></i> Exporter en Excel</a>
 @endpush
 @push('style')
     <style>
@@ -155,7 +162,7 @@
 @push('script')
     <script src="{{ asset('assets/fcadmin/js/vendor/datepicker.min.js') }}"></script>
     <script src="{{ asset('assets/fcadmin/js/vendor/datepicker.fr.js') }}"></script>
-<script src="{{ asset('assets/fcadmin/js/vendor/datepicker.en.js') }}"></script>
+    <script src="{{ asset('assets/fcadmin/js/vendor/datepicker.en.js') }}"></script>
 @endpush
 @push('script')
     <script>
@@ -163,18 +170,19 @@
             "use strict";
 
             $('.dates').datepicker({
-                maxDate:new Date(),
-                range:true,
-                multipleDatesSeparator:"-",
-                language:'en'
+                maxDate: new Date(),
+                range: true,
+                multipleDatesSeparator: "-",
+                language: 'en'
             });
 
-            let url=new URL(window.location).searchParams;
-            if(url.get('localite') != undefined && url.get('localite') != ''){
-                $('select[name=localite]').find(`option[value=${url.get('localite')}]`).attr('selected',true);
+            let url = new URL(window.location).searchParams;
+            if (url.get('localite') != undefined && url.get('localite') != '') {
+                $('select[name=localite]').find(`option[value=${url.get('localite')}]`).attr('selected', true);
             }
-            if(url.get('payment_status') != undefined && url.get('payment_status') != ''){
-                $('select[name=payment_status]').find(`option[value=${url.get('payment_status')}]`).attr('selected',true);
+            if (url.get('payment_status') != undefined && url.get('payment_status') != '') {
+                $('select[name=payment_status]').find(`option[value=${url.get('payment_status')}]`).attr('selected',
+                    true);
             }
 
         })(jQuery)
