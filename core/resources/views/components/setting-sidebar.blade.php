83dfc182f289231ca08a85e5c98d47ca77ae1fe3
jatachi <anguijathi@gmail.com>
2024-04-15 12:31:26 +0000

    update sideBarSetting

--- core/resources/views/components/setting-sidebar.blade.php
+++ core/resources/views/components/setting-sidebar.blade.php
@@ -19,30 +19,30 @@
 
     <!-- SETTINGS MENU START -->
     <ul class="settings-menu" id="settingsMenu">
-    <x-setting-menu-item :active="$activeMenu" menu="cooperative_settings" :href="route('manager.settings.cooperative-settings.index')" :text="__('Paramètres de Coopérative')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="section_settings" :href="route('manager.settings.section-settings.index')" :text="__('Paramètre de Section')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="localite_settings" :href="route('manager.settings.localite-settings.index')" :text="__('Paramètre de Localite')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="departement_settings" :href="route('manager.settings.departements.index')" :text="__('Paramètre Départements')"/> 
-    <x-setting-menu-item :active="$activeMenu" menu="designation_settings" :href="route('manager.settings.designations.index')" :text="__('Paramètre Désignations')"/> 
-    <x-setting-menu-item :active="$activeMenu" menu="magasinSection_settings" :href="route('manager.settings.magasinSection.index')" :text="__('Paramètre Magasins Sections')"/> 
-    <x-setting-menu-item :active="$activeMenu" menu="magasinCentral_settings" :href="route('manager.settings.magasinCentral.index')" :text="__('Paramètre Magasins Centraux')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="transporteur_settings" :href="route('manager.settings.transporteur.index')" :text="__('Paramètre des Transporteurs')"/> 
-    <x-setting-menu-item :active="$activeMenu" menu="vehicule_settings" :href="route('manager.settings.vehicule.index')" :text="__('Paramètre des Véhicules')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="remorque_settings" :href="route('manager.settings.remorque.index')" :text="__('Paramètre des Remorques')"/>  
-    <x-setting-menu-item :active="$activeMenu" menu="attendance_settings" :href="route('manager.settings.attendance-settings.index')" :text="__('app.menu.attendanceSettings')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="leave_settings" :href="route('manager.settings.leaves-settings.index')" :text="__('app.menu.leaveSettings')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="travauxDangereux_settings" :href="route('manager.settings.travauxDangereux.index')" :text="__('Paramètre Travaux Dangereux')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="travauxLegers_settings" :href="route('manager.settings.travauxLegers.index')" :text="__('Paramètre Travaux Legers')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="arretEcole_settings" :href="route('manager.settings.arretEcole.index')" :text="__('Paramètre Arrets Ecole')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="typeFormation_settings" :href="route('manager.settings.typeFormation.index')" :text="__('Paramètre Types de Formation')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="themeFormation_settings" :href="route('manager.settings.themeFormation.index')" :text="__('Paramètre Themes de Formation')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="sousThemeFormation_settings" :href="route('manager.settings.sousThemeFormation.index')" :text="__('Paramètre Sous theme formation')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="moduleFormationStaff_settings" :href="route('manager.settings.moduleFormationStaff.index')" :text="__('Paramètre Modules de Formation Staffs')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="themeFormationStaff_settings" :href="route('manager.settings.themeFormationStaff.index')" :text="__('Paramètre Themes de Formation Staffs')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="categorieQuestionnaire_settings" :href="route('manager.settings.categorieQuestionnaire.index')" :text="__('Paramètre Categorie Questionnaire')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="questionnaire_settings" :href="route('manager.settings.questionnaire.index')" :text="__('Paramètre Questionnaire')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="especeArbre_settings" :href="route('manager.settings.especeArbre.index')" :text="__('Paramètre Espèces Arbres')"/>
-    <x-setting-menu-item :active="$activeMenu" menu="typeArchive_settings" :href="route('manager.settings.typeArchive.index')" :text="__('Paramètre Type Archives')"/> 
+    <x-setting-menu-item :active="$activeMenu" menu="cooperative_settings" :href="route('manager.settings.cooperative-settings.index')" :text="__('Coopérative')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="section_settings" :href="route('manager.settings.section-settings.index')" :text="__('Section')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="localite_settings" :href="route('manager.settings.localite-settings.index')" :text="__('Localite')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="departement_settings" :href="route('manager.settings.departements.index')" :text="__('Départements')"/> 
+    <x-setting-menu-item :active="$activeMenu" menu="designation_settings" :href="route('manager.settings.designations.index')" :text="__('Désignations')"/> 
+    <x-setting-menu-item :active="$activeMenu" menu="magasinSection_settings" :href="route('manager.settings.magasinSection.index')" :text="__('Magasins Sections')"/> 
+    <x-setting-menu-item :active="$activeMenu" menu="magasinCentral_settings" :href="route('manager.settings.magasinCentral.index')" :text="__('Magasins Centraux')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="transporteur_settings" :href="route('manager.settings.transporteur.index')" :text="__('Transporteurs')"/> 
+    <x-setting-menu-item :active="$activeMenu" menu="vehicule_settings" :href="route('manager.settings.vehicule.index')" :text="__('Véhicules')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="remorque_settings" :href="route('manager.settings.remorque.index')" :text="__('Remorques')"/>  
+    <x-setting-menu-item :active="$activeMenu" menu="attendance_settings" :href="route('manager.settings.attendance-settings.index')" :text="__('Présence')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="leave_settings" :href="route('manager.settings.leaves-settings.index')" :text="__('Congé')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="travauxDangereux_settings" :href="route('manager.settings.travauxDangereux.index')" :text="__('Travaux Dangereux')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="travauxLegers_settings" :href="route('manager.settings.travauxLegers.index')" :text="__('Travaux Legers')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="arretEcole_settings" :href="route('manager.settings.arretEcole.index')" :text="__('Arrets Ecole')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="typeFormation_settings" :href="route('manager.settings.typeFormation.index')" :text="__('Types de Formation')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="themeFormation_settings" :href="route('manager.settings.themeFormation.index')" :text="__('Themes de Formation')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="sousThemeFormation_settings" :href="route('manager.settings.sousThemeFormation.index')" :text="__('Sous theme formation')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="moduleFormationStaff_settings" :href="route('manager.settings.moduleFormationStaff.index')" :text="__('Modules de Formation Staffs')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="themeFormationStaff_settings" :href="route('manager.settings.themeFormationStaff.index')" :text="__('Themes de Formation Staffs')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="categorieQuestionnaire_settings" :href="route('manager.settings.categorieQuestionnaire.index')" :text="__('Categorie Questionnaire')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="questionnaire_settings" :href="route('manager.settings.questionnaire.index')" :text="__('Questionnaire')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="especeArbre_settings" :href="route('manager.settings.especeArbre.index')" :text="__('Espèces Arbres')"/>
+    <x-setting-menu-item :active="$activeMenu" menu="typeArchive_settings" :href="route('manager.settings.typeArchive.index')" :text="__('Type Archives')"/> 
 
     </ul>
     <!-- SETTINGS MENU END -->
