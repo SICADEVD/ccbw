bc637f4097adb259e8171e4aaed8e3795054e957
jatachi <anguijathi@gmail.com>
2024-04-24 21:12:34 +0000

    update

--- core/app/Http/Controllers/ApiproducteurController.php
+++ core/app/Http/Controllers/ApiproducteurController.php
@@ -258,7 +258,7 @@ class ApiproducteurController extends Controller
         'sexe'  => 'required|max:255',
         'nationalite'  => 'required|max:255',
         'dateNaiss'  => 'required|max:255',
-        'phone1'  => ['required', 'regex:/^\d{10}$/', 'unique:producteurs,phone1'],
+        'phone1'  => ['required', 'regex:/^\d{10}$/'],
         'niveau_etude'  => 'required|max:255',
         'type_piece'  => 'required|max:255',
         'numPiece'  => 'required|max:255',
@@ -273,7 +273,7 @@ class ApiproducteurController extends Controller
         'autrePhone' => 'required_if:autreMembre,==,oui',
         'numCMU' => 'required_if:carteCMU,==,oui',
         'phone2' => Rule::when($request->autreMembre == 'oui', function () {
-          return ['required', 'regex:/^\d{10}$/', Rule::unique('producteurs', 'phone2')];
+          return ['required', 'regex:/^\d{10}$/'];
         }),
       ];
       $message = [
