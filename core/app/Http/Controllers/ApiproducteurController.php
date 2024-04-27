046d3e1352da60a65f4c3d8c255478ec96b2aca3
jularis <juliuskouame@gmail.com>
2024-04-27 01:50:03 +0000

    update

--- core/app/Http/Controllers/ApiproducteurController.php
+++ core/app/Http/Controllers/ApiproducteurController.php
@@ -307,7 +307,10 @@ class ApiproducteurController extends Controller
 
         if ($hasInfoProd) {
           $notify = "L'info existe déjà pour ce producteur. Veuillez apporter des mises à jour.";
-          return response()->json($notify, 201);
+          return response()->json([
+            'message'=>$notify,
+            'status'=>'refuse'
+          ]);
         }
       }
       $infoproducteur->producteur_id = $request->producteur_id;
